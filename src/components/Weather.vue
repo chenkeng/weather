5dfb274e81c880c689aee3654402995e19f8756e
chenkeng <chenchaohan@outlook.com>
2018-01-29 03:24:07 +0800

    add classify file

---
+++ src/components/Weather.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>这里是weather页面</p>
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Weather",
+  data () {
+    return {
+      msg: "welcome to weather forecast!"
+    };
+  }
+};
+</script>
+<style>
+
+</style>
+
+
